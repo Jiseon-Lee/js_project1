815d7c1ef3c49047dfa4f1bd9ad6f13075a877c8
Jiseon-Lee <1990831@naver.com>
2025-04-17 11:41:17 +0900

    fix url

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // 메인 카테고리 리스트 불러오기
 $(function(){
     $.ajax({
-        url: "json/main_nav.json",
+        url: "./json/main_nav.json",
         dataType: "json",
         success : function(data){
             if(data.length>0){
-                for(var i in data.slide(0, 8)){
+                for(var i in data.slice(0, 8)){
                     $("#main_nav_1 > ul").append(`
                         <li><a href="sub.html" target="_self">
                             <img src="${data[i].img}" alt="${data[i].name}">
@@ -14,7 +14,7 @@ $(function(){
                     `);
                 }
 
-                for(var i in data.slide(8)){
+                for(var i in data.slice(8)){
                     $("#main_nav_2 > ul").append(`
                         <li><a href="sub.html" target="_self">
                             <img src="${data[i].img}" alt="${data[i].name}">
