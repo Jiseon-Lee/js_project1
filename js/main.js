282c8a1267e23ddc14520be3b0d48a884dc6f6bf
Jiseon-Lee <1990831@naver.com>
2025-04-17 16:43:52 +0900

    img url change

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(function(){
         url: "./json/main_nav.json",
         dataType: "json",
         success : function(data){
+            console.log("main_nav");
+            console.log(data);
             if(data.length>0){
                 for(var i in data.slice(0, 8)){
                     $("#main_nav_1 > ul").append(`
@@ -14,11 +16,12 @@ $(function(){
                     `);
                 }
 
-                for(var i in data.slice(8, data.length)){
+                var nav_2 = data.slice(8);
+                for(var i in nav_2){
                     $("#main_nav_2 > ul").append(`
                         <li><a href="sub.html" target="_self">
-                            <img src="${data[i].img}" alt="${data[i].name}">
-                            <span>${data[i].name}</span>
+                            <img src="${nav_2[i].img}" alt="${nav_2[i].name}">
+                            <span>${nav_2[i].name}</span>
                         </a></li>
                     `);
                 }
@@ -28,30 +31,30 @@ $(function(){
 });
 
 // section_1 부담없는 알뜰 반찬 목록 불러오기
-document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const response = await fetch("./json/altelbanchan.json");
-        if (!response.ok) throw new Error("데이터를 불러오는 데 실패했습니다.");
+$(function(){
+    $.ajax({
+        url: "./json/altelbanchan.json",
+        dataType: "json",
+        success : function(data){
+            console.log("altelbanchan");
+            if(data.length>0){
+                for(var i in data){
+                    $("#section_1 > .banchan_list").append(`
+                        <div class="product">
+                            <a href="#">
+                                <img src="${data[i].main_img}" alt="${data[i].name}">
+                                <div class="prd_discription">
+                                    <h4>${data[i].name}</h4>
+                                    <p>${data[i].discription}</p>
+                                    <h5>${data[i].discount_price}원</h5>
+                                </div>
+                            </a>
+                        </div>
+                    `);
 
-        const data = await response.json();
-        if (data.length > 0) {
-            for(var i in data){
-                $("#section_1 > .banchan_list").append(`
-                    <div class="product">
-                        <a href="#">
-                            <img src="${data.main_img}" alt="${data.name}">
-                            <div class="prd_discription">
-                                <span class="prd_tag tag_${data.tag[0].tag_id}">${data.tag[0].tag_name}</span>
-                                <h4>${data.name}</h4>
-                                <p>${data.discription}</p>
-                                <h5>${data.discount_price}원</h5>
-                            </div>
-                        </a>
-                    </div>
-                `);
+
+                }
             }
         }
-    } catch (error) {
-        console.error("오류 발생:", error);
-    }
+    });
 });
